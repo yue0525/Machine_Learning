548ecdb668d6da6e2cc23eaae3ff9354ff99cb1b
yue0525 <tom890525@gmail.com>
2022-12-15 13:52:15 +0800

    add: kernel

--- HW6/6.py
+++ HW6/6.py
@@ -5,26 +5,32 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 from PIL import Image, ImageColor
 import os
+import cv2
 
 
 def load():
-    img1 = Image.open("Machine_Learning\HW6\image1.png")
-    img2 = Image.open("Machine_Learning\HW6\image1.png")
-    data1 = np.array(img1)
-    data2 = np.array(img2)
-    # print(data1.shape)
-    dataC = data1.reshape((data1.shape[0]*data1.shape[1], data1.shape[2]))
-    # spatial data: coordinate for each pixel
-    dataS = np.array([(i, j) for i in range(data1.shape[0])
-                     for j in range(data1.shape[1])])
-    print(img1.size)
-    return data1, data2
+    # img1 = Image.open("Machine_Learning\HW6\image1.png")
+    # img2 = Image.open("Machine_Learning\HW6\image2.png")
+    imgcv2_1 = cv2.imread('Machine_Learning\HW6\image1.png')
+    # print(imgcv2_1.shape)
+    data_Color = imgcv2_1.reshape(-1, imgcv2_1.shape[2])
+    data_Spatial = np.array([(i, j) for i in range(
+        imgcv2_1.shape[0])for j in range(imgcv2_1.shape[1])])
+    return data_Color, data_Spatial
 
 
-def kernel_k_means():
-    return
+def kernel_k_means(data_Color, data_Spatial):
+    s = 0.001
+    c = 0.001
+    gram = np.exp(-s * cdist(data_Spatial, data_Spatial, 'sqeuclidean'))
+    gram *= np.exp(-c * cdist(data_Color, data_Color, 'sqeuclidean'))
+    return gram
 
 
 if __name__ == "__main__":
-    data1, data2 = load()
-    data1, data2 = load()
+    data_Color, data_Spatial = load()
+    Gram = kernel_k_means(data_Color, data_Spatial)
+    print(Gram.shape)
+    # data1, data2 = load()
+    # print(data1)
+    # data1, data2 = load()
